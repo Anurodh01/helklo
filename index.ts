525831682e32f38c40e76ed34f3ee628329b59a3
anurodhsingh <anurodh.singh@nagarro.com>
2025-04-18 19:29:48 +0530

    subscription service added

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ console.log('Index')
 console.log("Added Another line in third commit")
 console.log("Added another line in fourth commit")
 console.log("Added another line in fivth commit")
+
+console.log('Subscription Service is about to start')
